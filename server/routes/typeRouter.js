e46cd684d910e4f0fc582a32f027c6ac41f162ad
Nikita Zlobin <zlo-nic@mail.ru>
2022-02-03 22:38:41 +0500

    REMOVE

--- server/routes/typeRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const typeController = require('../controllers/typeController')
-
-router.post('/', typeController.create)
-router.get('/', typeController.getAll)
-
-module.exports = router
